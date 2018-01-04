93c689ed083d03a6d7a874010861bf0e3beb76f6
HongSh1991 <hjmrezl@outlook.com>
2018-01-04 15:45:20 +0800

    add picovr teleport sdk

---
+++ testTeleport/Assets/Script/teleportmove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pvr_UnitySDKAPI;
+
+public class teleportmove : MonoBehaviour
+{
+
+
+	private Vector3 cameraposition;
+	private Transform pvr_unitysdkposition;
+	public ParabolicPointer parabolicPointer;
+	// Use this for initialization
+	void Start()
+	{
+
+		pvr_unitysdkposition = GameObject.Find("Pvr_UnitySDK").transform;
+		cameraposition = pvr_unitysdkposition.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (parabolicPointer.PointOnNavMesh)
+		{
+			if (Controller.UPvr_GetKeyDown(Pvr_KeyCode.TOUCHPAD) || Input.GetMouseButtonDown(0))
+			{
+				pvr_unitysdkposition.position = parabolicPointer.SelectedPoint + Vector3.up;
+			}
+
+		}
+	}
+}
