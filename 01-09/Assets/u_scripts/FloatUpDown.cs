5a398179579deff795fc57ae9615b022b14767f5
HongSh1991 <hjmrezl@outlook.com>
2018-01-10 16:14:01 +0800

    新增物体浮动效果

---
+++ 01-09/Assets/u_scripts/FloatUpDown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatUpDown : MonoBehaviour {
+
+	public float radian = 0;//弧度
+	public float perChangeRadian = 0.03f;//每次变化的弧度
+	public float radius = 0.8f;//半径
+	private Vector3 originPos;//开始时的坐标
+
+	// Use this for initialization
+	void Start () {
+		originPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		radian += perChangeRadian;
+		float dy = Mathf.Cos(radian) * radius;//每次上下变换的幅度
+		transform.position = originPos + new Vector3(0, dy, 0);
+	}
+}
---
+++ 01-09/Assets/u_scripts/HideTips.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideTips : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameObject.Find("秒表").transform.position.y > 1.032)
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
